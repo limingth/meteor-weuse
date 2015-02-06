a91b9c4776cca0542257e01d3aaa6b70571511a6
farcryzry <farcryzry@gmail.com>
2015-02-05 16:56:59 -0800

    fix the problem of infinite call of setting login position

--- server/methods.js
+++ server/methods.js
@@ -1,26 +1,20 @@
 Meteor.methods({
-  // 'test':function () {
-  //   console.log(Meteor.user().profile);
-  // },
   'set-user-geohash': function (geohash) {
     // TODO: test valide geohash
     check(geohash, String);
     Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.geohash":geohash, updatedAt:new Date() }});
   },
-
   'unset-user-geohash': function () {
     Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.geohash":undefined}});
   },
-
-  'set-user-login-pos': function(pos) {
-    Meteor.users.update({_id:Meteor.user()._id}, {$set:{"loginpos":pos}});
-  },
-
   'set-user-login-addr': function(addr) {
     Meteor.users.update({_id:Meteor.user()._id}, {$set:{"loginaddr":addr}});
   },
-
   'set-user-login-zip': function(zip) {
-    Meteor.users.update({_id:Meteor.user()._id}, {$set:{"loginzip":zip}});
+    Meteor.users.update({_id: Meteor.user()._id}, {$set: {"loginzip": zip}});
+  },
+  'set-user-login-pos': function(pos) {
+    var currentGeohash = geohash.encode(pos.lat, pos.lng, 5);
+    Meteor.users.update({_id:Meteor.user()._id}, {$set:{"loginpos":pos, "profile.geohash":currentGeohash, updatedAt:new Date()}});
   }
 });
